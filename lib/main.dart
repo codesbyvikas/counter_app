04e289ab24ab8bd6342cda84620223d40d08fc04
Vikas Kewat <vikaskewat025@gmail.com>
2024-04-03 21:31:17 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:counter_app/bloc/counter/counter_bloc.dart';
+import 'package:counter_app/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: MaterialApp(
+        title: "Counter App",
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true),
+        home: const CounterScreen(),
       ),
     );
   }
